f524c214fcadd51f99ae82376037fccd193a4ae1
Michał Tamaka <tamahawk@wp.pl>
2022-11-27 12:43:04 +0100

    adding new task

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
             done: false
         },
         {
-            content: "pić wino",
+            content: "pić gin z tonikiem",
             done: true
         },
     ];
@@ -15,7 +15,8 @@
 
         for (const task of tasks) {
             htmlString += `
-            <li>
+            <li ${task.done ? "class=\"task__content\"" : ""}
+            >
             ${task.content}
             </li>
             `;
@@ -24,8 +25,32 @@
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
