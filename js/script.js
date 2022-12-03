e59b64e55b801aae2c428161fd9b9e9f9363130e
Michał Tamaka <tamahawk@wp.pl>
2022-12-03 19:16:30 +0100

    added focus to input

--- js/script.js
+++ js/script.js
@@ -68,14 +68,16 @@
 
     const onFormSubmit = (event) => {
         event.preventDefault();
-
-        const newTaskContent = document.querySelector(".js-newTask").value.trim();
-
-        if (newTaskContent === "") {
-            return;
+        
+        const newTaskElement = document.querySelector(".js-newTask");
+        const newTaskContent = newTaskElement.value.trim();
+        
+        if (newTaskContent !== "") {
+            addNewTask(newTaskContent);
+            newTaskContent.value = "";
         }
 
-        addNewTask(newTaskContent);
+        newTaskContent.focus();
     };
 
     const init = () => {
